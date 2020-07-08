4aa4b96e8bbd79988f9188f5fe11c3d5fc5a32f6
Priyanshi Patel <silver.puzzle.10@gmail.com>
2020-07-08 16:52:29 -0400

    Added stuff

--- sketch.js
+++ sketch.js
@@ -1,76 +1,88 @@
-var easycam;
-let fr = 20;
-let size = 50;
-let increment = 20;
+let center = [0,0,0]
 
-var state1 = {
-  distance : 100,
-  center   : [0,0,0],
-  rotation : [1,0.5,0.25,0],
-};
+const s = (p) => {
+  var easycam
+  let fr = 20
+  let size = 50
+  let increment = 20
+  var length = 1000
+  var myFont
+  let equation = []
 
-function setup() {
-  createCanvas(0.9*windowWidth,0.9*windowHeight, WEBGL);
-  easycam = new Dw.EasyCam(this._renderer, state1);
-  easycam.setRotation(Dw.Rotation.create({angles_xyz:[8*PI, 8*PI, 9*PI]}), 50000);
-  frameRate(fr);
-}
-function draw() {
+  var state1 = {
+    distance : 200,
+    center   : [0,0,0],
+    rotation : [1,0.5,0.25,0],
+  };
 
-  // rotation animation
-  easycam.setRotation(Dw.Rotation.create({angles_xyz:[8*PI, 8*PI, 9*PI]}), 50000);
-  background(10);
+  p.preload = function() {
+    myFont = p.loadFont('./fonts/Inconsolata-Black.ttf')
+  }
+
+  p.setup = function() {
+    let canvas = p.createCanvas(0.7*p.windowWidth,p.windowHeight, p.WEBGL)
+    canvas.position(0,0)
+    easycam = new Dw.EasyCam(this._renderer, state1)
+    easycam.setRotation(Dw.Rotation.create({angles_xyz:[8*p.PI, 8*p.PI, 9*p.PI]}), 50000)
+    p.frameRate(fr)
+  }
+
+  p.draw = function() {
+
+    easycam.setRotation(Dw.Rotation.create({angles_xyz:[8*p.PI, 8*p.PI, 9*p.PI]}), 50000)
+    p.background(15)
+    draw_grid()
+    draw_field()
+  }
   
-  // x,y,z axis
-  strokeWeight(0.8)
-  push();
-  stroke(250,0,0);
-  line(-200,0,0,200,0,0);
-  stroke(0,250,0);
-  line(0,-200,0,0,200,0);
-  stroke(0,0,250);
-  line(0,0,-200,0,0,200);
-  pop();
-  
-  // vector field F(x,y,z) = <x,y,z>
-  // vector field F(x,y,z) = <-y,x,z>
-  for (var k=-1*size; k<=size; k+=increment)
-    for (var i = -1*size; i <= size; i += increment) {
-      for (var j = -1*size; j<= size; j+=increment) {
-        var ball = new Arrow(i,j,k);
-        ball.draw();
-        ball.drawArrow();
+  draw_grid = function() {
+    p.strokeWeight(0.8)
+    p.push()
+    // x axis: red
+    p.stroke(250,0,0)
+    p.line(-1*length,-1*center[1],-1*center[2],length,-1*center[1],-1*center[2])
+    // y axis: green
+    p.stroke(0,250,0)
+    p.line(-1*center[0],-1*length,-1*center[2],-1*center[0],length,-1*center[2])
+    // z axis: blue 
+    p.stroke(0,0,250)
+    p.line(-1*center[0],-1*center[1],-1*length,-1*center[0],-1*center[1],length)
+    
+    for (var i=-1*length ; i<length; i+=20){
+      if (i != 0) {
+        p.strokeWeight(0.25)
+        p.stroke(255,0,0)
+        p.line(i,-1*center[1],-1.5-center[2],i,-1*center[1],1.5-center[2])
+        p.line(i,-1.5-center[1],-1*center[2],i,1.5-center[1],-1*center[2])
+        p.stroke(0,255,0)
+        p.line(-1.5-center[0],i,-1*center[2],1.5-center[0],i,-1*center[2])
+        p.line(-1*center[0],i,-1.5-center[2],-1*center[0],i,1.5-center[2])
+        p.stroke(0,0,255)
+        p.line(-1*center[0],-1.5-center[1],i,-1*center[0],1.5-center[1],i)
+        p.line(-1.5-center[0],-1*center[1],i,1.5-center[0],-1*center[1],i)
       }
     }
-
-
-    
-}
-
-
-class Arrow {
-  constructor(x,y,z) {
-    this.x = x;
-    this.y = y;
-    this.z = z; 
-    this.r = 0.1;
+   
   }
 
-  draw() {
-    push();
-    point(this.x,this.y,this.z);
-    pop();
-  }
+  draw_field = function() {
+    // vector field F(x,y,z) = <x,y,z>
+    // vector field F(x,y,z) = <-y,x,z>
+    for (var k = (-1*size); k <= (size); k+=increment) {
+      for (var i = (-1*size); i <= (size); i += increment) {
+        for (var j = (-1*size); j <= (size); j+=increment) {
+          p.strokeWeight(5)
+          p.stroke(255,255,255)
+          p.point(i,j,k)
+          p.strokeWeight(1)
+          p.line(i,j,k,1.2*i,1.2*j,1.2*k)
 
-  drawArrow() {
-    push();
-    stroke(195,195,195);
-    strokeWeight(0.5);
-    line(this.x, this.y, this.z, 1.2*this.x, 1.2*this.y, 1.2*this.z);
-    strokeWeight(5)
-    point(1.2*this.x, 1.2*this.y, 1.2*this.z);
-    pop();
+        }
+      }
+    }
   }
 
 }
 
+
+let myp5 = new p5(s)
